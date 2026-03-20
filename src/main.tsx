9129f78030dafc22f139edbba5b67f8436bcae7d
Wiler Mariñez <wilersoriano@gmail.com>
2026-03-20 20:34:54 +0100

    feat: add Fira Code Variable font and update styling for improved typography

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource-variable/fira-code/index.css'
 import 'reactflow/dist/style.css'
 import './index.css'
 import App from './App.tsx'
