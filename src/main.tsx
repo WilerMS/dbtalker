d71da0ba5b8ff9f1cdbbd2eabca27cef0dfa138d
Wiler Mariñez <wilersoriano@gmail.com>
2026-03-21 01:05:10 +0100

    style: wrap App component with QueryClientProvider and BrowserRouter for improved data management and routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import '@fontsource-variable/fira-code/index.css'
 import 'reactflow/dist/style.css'
 import './index.css'
@@ -11,8 +13,14 @@ if (!rootElement) {
   throw new Error('Root element not found')
 }
 
+const queryClient = new QueryClient()
+
 createRoot(rootElement).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
 )
